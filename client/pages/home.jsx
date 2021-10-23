7eb4cc8fe3e65ed8337e96bf055de296ba962650
febriadj <iamfebriadji@gmail.com>
2021-10-23 21:07:23 +0700

    :hammer: Update home page

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import Banner from '../containers/home/banner';
+import React, { useEffect, useState } from 'react';
 import style from '../styles/pages/home.css';
 
+import Navbar from '../containers/navbar';
+import Banner from '../containers/home/banner';
+import GridBox from '../containers/home/gridbox';
+import Profile from '../containers/home/profile';
+import Footer from '../containers/footer';
+
 function Home() {
+  const [title, setTitle] = useState(document.title);
+
+  useEffect(() => {
+    setTitle('Home - @febriadj');
+    document.title = title;
+  }, [
+    title,
+  ]);
+
   return (
     <div className={style.home}>
-      <div className={style['home-wrap']}>
-        <Banner style={style} />
-      </div>
+      <Navbar />
+      <Banner style={style} />
+      <GridBox style={style} />
+      <Profile style={style} />
+      <Footer />
     </div>
   );
 }
