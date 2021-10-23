a13a85952aea1c7eaee1be9200985e9bfd368b18
febriadj <iamfebriadji@gmail.com>
2021-10-23 21:13:51 +0700

    :hammer: Add navbar container

--- client/containers/navbar.jsx
+++ client/containers/navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import style from '../styles/containers/navbar.css';
 
 function Navbar() {
   return (
     <div className={style.navbar}>
-      <p>navbar</p>
+      <div className={style.logo}>
+        <box-icon type="reguler" name="meh-blank" color="#ffffff"></box-icon>
+      </div>
+      <div className={style.menu}>
+        <Link to="/articles" className={style.link}><h3>Articles</h3></Link>
+        <Link to="/works" className={style.link}><h3>Works</h3></Link>
+        <Link to="/contact" className={style.link}><h3>Contact</h3></Link>
+      </div>
     </div>
   );
 }
