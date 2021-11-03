198fae5cf9e8eccc8ed7d8e6fb017e907c2989eb
febriadj <iamfebriadji@gmail.com>
2021-11-03 04:22:55 -0700

    :hammer: Add cookie container

---
+++ client/containers/cookie.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Cookie({ style, handleCookie }) {
+  const handleSetCookie = () => {
+    document.cookie = `client=${Date.now()}.${1000 - Math.random() * 9000}`;
+    setTimeout(() => handleCookie(), 500);
+  }
+
+  return (
+    <div className={style.cookie}>
+      <div className={style['cookie-wrap']}>
+        <div className={style.box}>
+          <h3 className={style.title}>This website use Cookies.</h3>
+          <p className={style.paragraf}>
+            <span>
+              I use cookies to collect and analyse information
+              on site performance and usage, to provide social media features and to enhance and
+            </span>
+            <span>customise content and advertisements. </span>
+            <Link to="/cookie" className={style.link}>learn more</Link>
+          </p>
+          <div className={style.action}>
+            <button className={style.btn} onClick={handleSetCookie}>Decline</button>
+            <button className={style.btn} onClick={handleSetCookie}>Allow Access</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cookie;
