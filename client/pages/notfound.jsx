6783978bcfdd787cf849e164b0d2edbf56d3d095
febriadj <iamfebriadji@gmail.com>
2021-11-06 00:47:42 -0700

    :hammer: Update 404 page

--- client/pages/notfound.jsx
+++ client/pages/notfound.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect, useSelector } from 'react-redux';
 import style from '../styles/pages/notfound.css';
 
 function NotFound() {
   const [title, setTitle] = useState(document.title);
+  const props = useSelector((state) => state);
 
   useEffect(() => {
     setTitle('@febriadj - 404 Page Not Found');
@@ -17,17 +19,33 @@ function NotFound() {
       <div className={style['notfound-wrap']}>
         <div className={style.banner}>
           <h1 className={style.title}>Oops. <span>404</span></h1>
-          <h3 className={style.paragraf}>
-            I can't find the page you're looking for. You don't need to be confused,
-          </h3>
-          <h3 className={style.paragraf}>
-            please return to the main page by pressing the button below
-          </h3>
-          <Link to="/" className={style.link}>Home</Link>
+          <p className={style.paragraf}>
+            I can't find the page you're looking for. You don't need to be confused.
+          </p>
+          <p className={style.paragraf}>
+            Please return to the main page by pressing the button below
+          </p>
+          <Link
+            className={style.link}
+            to="/"
+            style={{
+              borderColor: props.darkMode ? '#ffffffdd' : null,
+              color: props.darkMode ? '#ffffffdd' : null,
+            }}
+          >
+            Go to Home Page
+          </Link>
         </div>
       </div>
     </div>
   );
 }
 
-export default NotFound;
+const mapStateToProps = (state) => {
+  const response = {
+    darkMode: state.darkMode,
+  }
+  return response;
+}
+
+export default connect(mapStateToProps)(NotFound);
