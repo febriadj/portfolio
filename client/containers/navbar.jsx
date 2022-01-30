104019e7ad1a902825ffa83e8b42c654a1ed0656
febriadj <iamfebriadji@gmail.com>
2022-01-30 18:03:39 +0700

    :hammer::lipstick: Update navbar container & style

--- client/containers/navbar.jsx
+++ client/containers/navbar.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import style from '../styles/containers/navbar.css';
 
 function Navbar() {
   const { darkmode } = useSelector((state) => state);
+  const [hamBtn, setHamBtn] = useState(false);
 
   return (
     <div className={style.navbar}>
@@ -16,6 +17,26 @@ function Navbar() {
           <Link to="/articles" className={`${style.link} ${darkmode && style.dark}`}>Articles</Link>
           <Link to="/works" className={`${style.link} ${darkmode && style.dark}`}>Works</Link>
           <Link to="/contact" className={`${style.link} ${darkmode && style.dark}`}>Contact</Link>
+        </div>
+        <div className={style.action}>
+          <button type="button" className={`${style.btn} ${style['auth-btn']}`}>
+            <box-icon name="user" color={darkmode ? '#ffffffdd' : '#000000dd'}></box-icon>
+          </button>
+          <button type="button" className={`${style.btn} ${style['book-btn']}`}>
+            <box-icon name="book-bookmark" color={darkmode ? '#ffffffdd' : '#000000dd'}></box-icon>
+          </button>
+          <button
+            type="button"
+            className={`${style.btn} ${style['ham-btn']}`}
+            onClick={() => {
+              setHamBtn((prev) => !prev);
+            }}
+          >
+            <span className={`${style.strip} ${style.strip1} ${hamBtn && style.active}`}></span>
+            <span className={`${style.strip} ${style.strip2} ${hamBtn && style.active}`}></span>
+            <span className={`${style.strip} ${style.strip3} ${hamBtn && style.active}`}></span>
+            <span className={`${style.strip} ${style.strip4} ${hamBtn && style.active}`}></span>
+          </button>
         </div>
       </div>
     </div>
