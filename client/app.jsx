b42567b7b669a52133bccca49844f922b9f07f01
febriadj <iamfebriadji@gmail.com>
2022-01-31 18:51:20 +0700

    :passport_control: Add user authentication handling

--- client/app.jsx
+++ client/app.jsx
@@ -8,8 +8,10 @@ import * as page from './pages';
 import * as comp from './components';
 
 function App() {
-  const { darkmode } = useSelector((state) => state);
+  const isDev = process.env.NODE_ENV === 'development';
+
   const dispatch = useDispatch();
+  const { darkmode, isLoggedIn } = useSelector((state) => state);
 
   const handleDarkMode = () => {
     if (darkmode) {
@@ -25,9 +27,51 @@ function App() {
     }
   }
 
+  const handleGetUser = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const url = isDev ? 'http://localhost:8080/api/users' : '/api/users';
+
+      const request = await (await fetch(url, {
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })).json();
+
+      if (!request.success) throw request;
+
+      dispatch({
+        type: 'counter/user',
+        payload: request.data,
+      });
+    }
+    catch (error0) {
+      console.error(error0.message);
+    }
+  }
+
   useEffect(() => {
     document.body.style = darkmode ? 'background: #1a1b1e; color: #ffffffdd; ' : null;
   }, [darkmode]);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      dispatch({
+        type: 'counter/isLoggedIn',
+        payload: true,
+      });
+
+      handleGetUser();
+    } else {
+      dispatch({
+        type: 'counter/isLoggedIn',
+        payload: false,
+      });
+    }
+  }, [isLoggedIn]);
 
   return (
     <BrowserRouter>
