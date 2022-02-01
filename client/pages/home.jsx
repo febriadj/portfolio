b19fe7d1ca9a5f5e840f382ad417cb12461c061e
febriadj <iamfebriadji@gmail.com>
2022-02-01 23:15:48 +0700

    :building_construction: Make changes jsx structure in the home page

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import style from '../styles/pages/home.css';
 
 import * as comp0 from '../components';
@@ -6,45 +7,57 @@ import * as comp1 from '../components/home';
 import * as comp2 from '../components/auth';
 
 function Home() {
-  const [title, setTitle] = useState(document.title);
+  const dispatch = useDispatch();
+  const { darkmode } = useSelector((state) => state);
+
   const [menuBarIsOpen, setMenuBarIsOpen] = useState(false);
   const [auth, setAuth] = useState({
     login: false,
     register: false,
   });
 
+  const handleDarkMode = () => {
+    dispatch({
+      type: 'counter/darkmode',
+      payload: !darkmode,
+    });
+  }
+
   useEffect(() => {
-    setTitle('@febriadj - Home');
-    document.title = title;
-  }, [
-    title,
-  ]);
+    document.title = '@febriadj - Home';
+  });
 
   return (
-    <div className={style.home}>
-      <comp0.loading />
-      <comp0.navbar
-        setMenuBarIsOpen={setMenuBarIsOpen}
-        setAuth={setAuth}
-      />
-      <comp0.menubar
-        location="/"
-        menuBarIsOpen={menuBarIsOpen}
-      />
-      <comp2.login
-        loginFormIsOpen={auth.login}
-        setAuth={setAuth}
-      />
-      <comp2.register
-        registerFormIsOpen={auth.register}
-        setAuth={setAuth}
-      />
-      <div className={style['home-wrap']}>
-        <comp1.banner />
-        <comp1.gridbox />
-        <comp1.profile />
+    < >
+      <div className={style.home}>
+        <comp0.loading />
+        <comp0.navbar
+          setMenuBarIsOpen={setMenuBarIsOpen}
+          setAuth={setAuth}
+        />
+        <comp0.menubar
+          location="/"
+          menuBarIsOpen={menuBarIsOpen}
+        />
+        <comp2.login
+          loginFormIsOpen={auth.login}
+          setAuth={setAuth}
+        />
+        <comp2.register
+          registerFormIsOpen={auth.register}
+          setAuth={setAuth}
+        />
+        <div className={style['home-wrap']}>
+          <comp1.banner />
+          <comp1.gridbox />
+          <comp1.profile />
+        </div>
       </div>
-    </div>
+      <comp0.footer
+        darkmode={darkmode}
+        handleDarkMode={handleDarkMode}
+      />
+    </>
   );
 }
 
