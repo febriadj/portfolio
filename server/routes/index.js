c1ccad40237287b4c762f0bc8575b22e6c7621bb
febriadj <iamfebriadji@gmail.com>
2022-02-03 23:57:20 +0700

    :hammer: Update server routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 const authenticate = require('../middleware/auth');
 const user = require('../controllers/user');
+const article = require('../controllers/article');
 
 router.post('/users/register', user.register);
 router.post('/users/login', user.login);
 router.get('/users', authenticate, user.findOne);
+
+router.post('/articles', authenticate, article.insert);
+router.get('/articles', authenticate, article.findAll);
+router.delete('/articles', authenticate, article.delete);
 
 module.exports = router;
